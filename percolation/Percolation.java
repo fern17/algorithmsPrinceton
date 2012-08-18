10e17c0c22938e7a9996fae11e4a92861a9e26ab
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2012-08-18 04:40:05 -0300

    Se comprobo el estilo y que compilen. Falta debuggear

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,76 +1,88 @@
-import java.lang.*;
+//import java.lang.IndexOutOfBoundsException;
 public class Percolation {
     private WeightedQuickUnionUF sites; //quickunion
     private int N;                      //number of sites
-    private int cC(int i, int j){ //converts a coordinate (i,j) into a array index
-        if(i <= 0 || i > this.N) throw new IndexOutOfBoundsException("row index i out of bounds");
-        if(j <= 0 || j > this.N) throw new IndexOutOfBoundsException("row index j out of bounds");
-        return 1+this.N*(i-1)+j-1; //calculates the position in the quickunion
-    }
     
-    public Percolation(int N){              // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {              
+        // create N-by-N grid, with all sites blocked
         this.sites = new WeightedQuickUnionUF(N*N+2);
         this.N = N;
         
-        for (int i = 1; i <= N; i++){
-            this.sites.union(0,i); //unites all sites with the source
+        for (int i = 1; i <= N; i++) {
+            //unites all sites with the source
+            this.sites.union(0, i); 
         }
         
-        for (int i = N*N-N+1; i <= N*N; i++){
-            this.sites.union(N*N+1,i); //unites all sites with the destiny
+        for (int i = N*N-N+1; i <= N*N; i++) {
+            //unites all sites with the destiny
+            this.sites.union(N*N+1, i);
         }
     }
+    
+    private int cC(int i, int j) { 
+        //converts a coordinate (i,j) into a array index
+        
+        if (i <= 0 || i > this.N) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > this.N) throw 
+            new IndexOutOfBoundsException("row index j out of bounds");
+        
+        //calculates the position in the quickunion
+        return 1+this.N*(i-1)+j-1; 
+    }
 
-    public void open(int i, int j){         // open site (row i, column j) if it is not already
-        if(i <= 0 || i > this.N) throw new IndexOutOfBoundsException("row index i out of bounds");
-        if(j <= 0 || j > this.N) throw new IndexOutOfBoundsException("row index j out of bounds");
+    public void open(int i, int j) {         
+        // open site (row i, column j) if it is not already
+        if (i <= 0 || i > this.N) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > this.N) throw 
+            new IndexOutOfBoundsException("row index j out of bounds");
 
-        if(isOpen(i,j))
+        if (isOpen(i, j))
             return;
-        int pos = cC(i,j);
-        this.sites.union(pos,pos); //unites with itself
-        if((i != 1) && (this.sites.isOpen(i-1,j))){ //if this is not at the upper bound
+        int pos = cC(i, j);
+        this.sites.union(pos, pos); //unites with itself
+        if ((i != 1) && (isOpen(i-1, j))) { 
+            //if this is not at the upper bound
             this.sites.union(pos, pos-N);
         }
-        if((i != N) && (this.sites.isOpen(i+1,j))){ //if this is not at the lower bound
+        if ((i != N) && (isOpen(i+1, j))) { 
+            //if this is not at the lower bound
             this.sites.union(pos, pos+N);
         }
-        if((j != 1) && (this.sites.isOpen(i,j-1))){ //if not left bound
-            this.sites.union(pos,pos-1);
+        if ((j != 1) && (isOpen(i, j-1))) { 
+            //if not left bound
+            this.sites.union(pos, pos-1);
         }
-        if((j != N) && (this.sites.isOpen(i,j+1))){ //if not right bound
-            this.sites.union(pos,pos+1);
+        if ((j != N) && (isOpen(i, j+1))) {
+            //if not right bound
+            this.sites.union(pos, pos+1);
         }
     }
 
-    public boolean isOpen(int i, int j){    // is site (row i, column j) open?
-        if(i <= 0 || i > this.N) throw new IndexOutOfBoundsException("row index i out of bounds");
-        if(j <= 0 || j > this.N) throw new IndexOutOfBoundsException("row index j out of bounds");
+    public boolean isOpen(int i, int j) {    
+        // is site (row i, column j) open?
+        if (i <= 0 || i > this.N) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > this.N) throw 
+            new IndexOutOfBoundsException("row index j out of bounds");
     
-        int pos = cC(i,j); //gets array position
-        return this.sites.connected(pos,pos);
-       /* 
-        int pos = cC(i,j); //gets array position
-        if ((i != 1) && (this.sites.connected(pos,pos-N))) 
-            return true;
-        if ((i != N) && (this.sites.connected(pos,pos+N))) 
-            return true;
-        if ((j != 1) && (this.sites.connected(pos,pos-1))) 
-            return true;
-        if ((j != N) && (this.sites.connected(pos,pos+1))) 
-            return true;
-        return false;
-        */
+        int pos = cC(i, j); //gets array position
+        return this.sites.connected(pos, pos);
     }
 
-    public boolean isFull(int i, int j){    // is site (row i, column j) full?
-        if(i <= 0 || i > this.N) throw new IndexOutOfBoundsException("row index i out of bounds");
-        if(j <= 0 || j > this.N) throw new IndexOutOfBoundsException("row index j out of bounds");
-        int pos = cC(i,j);
+    public boolean isFull(int i, int j) {    
+        // is site (row i, column j) full?
+        if (i <= 0 || i > this.N) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > this.N) throw 
+            new IndexOutOfBoundsException("row index j out of bounds");
+        int pos = cC(i, j);
         return this.sites.connected(0, pos);
     }
     
-    public boolean percolates(){            // does the system percolate?
+    public boolean percolates() {            
+        // does the system percolate?
         return this.sites.connected(0, N*N+1);
     }
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -1,52 +1,57 @@
-import java.lang.*;
 public class PercolationStats {
     private double[] allruns;
 
-    public PercolationStats(int N, int T){    // perform T independent computational experiments on an N-by-N grid
-        if((N <= 0) || (T <= 0)) throw new IllegalArgumentException("T or N are illegal arguments");
+    public PercolationStats(int N, int T) {    
+        // perform T independent computational experiments on an N-by-N grid
+        if ((N <= 0) || (T <= 0)) throw 
+            new IllegalArgumentException("T or N are illegal arguments");
         allruns = new double[T];
         
-        for (int i = 0; i < T; i++){
+        for (int i = 0; i < T; i++) {
             Percolation per = new Percolation(N);
             int steps = 0;
             Stopwatch clock = new Stopwatch();
-            while((!per.percolates()) && (steps < N*N)){ //prevents infinite loop
+            while ((!per.percolates()) && (steps < N*N)) { 
+                //prevents infinite loop
                 int x = 1 + StdRandom.uniform(N);
                 int y = 1 + StdRandom.uniform(N);
-                while(!per.isOpen(x,y)){ //if not is open, regenerates random until it does
+                while (!per.isOpen(x, y)) { 
+                    //if not is open, regenerates random until it does
                     x = 1 + StdRandom.uniform(N);
                     y = 1 + StdRandom.uniform(N);
                 }
 
-                per.open(x,y); //open site
+                per.open(x, y); //open site
                 steps++; //increases the number of runs    
             }
 
             double elapsed = clock.elapsedTime();
-            double threshold = (double)steps/N;
+            double threshold = (double) steps/N;
             this.allruns[i] = threshold; 
         }
     }
 
-    public double mean(){                     // sample mean of percolation threshold
-        return StdStats.mean(allruns,0,allruns.length);
+    public double mean() {
+        // sample mean of percolation threshold
+        return StdStats.mean(allruns, 0, allruns.length);
     }
 
-    public double stddev(){                   // sample standard deviation of percolation threshold
-        return StdStats.stddev(allruns,0,allruns.length);
+    public double stddev() {                   
+        // sample standard deviation of percolation threshold
+        return StdStats.stddev(allruns, 0, allruns.length);
     }
 
-    public static void main(String[] args){   // test client
-        if (args.length > 1){
+    public static void main(String[] args) {   // test client
+        if (args.length > 1) {
             int N = Integer.parseInt(args[0]);
             int T = Integer.parseInt(args[1]);
-        }
 
-        PercolationStats ps = new PercolationStats(N,T);
-        double media    = ps.mean();
-        double devest   = ps.stddev();
-        double inicio   = media - 1.96*devest/Math.sqrt(T);
-        double fin      = media + 1.96*devest/Math.sqrt(T);
+            PercolationStats ps = new PercolationStats(N, T);
+            double media    = ps.mean();
+            double devest   = ps.stddev();
+            double inicio   = media - 1.96*devest/Math.sqrt(T);
+            double fin      = media + 1.96*devest/Math.sqrt(T);
+        }
     }
 }
 
